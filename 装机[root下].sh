3330635cf913e259d4e4c4fd2e04dd46557fb5f1
will <ljclg_1516@foxmail.com>
2016-04-26 18:43:48 +0800

    add some

--- 装机[root下].sh
+++ 装机[root下].sh
@@ -6,7 +6,7 @@ echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >>
 #.vimrc同理
 
 #总汇
-#apt-get install build-essential vim-gtk ctags cscope astyle git ssh manpages-zh
+#apt-get install build-essential vim-gtk ctags cscope astyle git ssh manpages-zh cmake
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #/etc/init.d/ssh start
 #cd ~; ssh-keygen -t rsa
@@ -32,6 +32,9 @@ apt-get install ssh    #其实就是openssh(sever&client)
 echo "开始安装 中文man"
 apt-get install manpages-zh
 
+#cmake
+apt-get install cmake
+
 #pycharm
 
 #VSCode
@@ -40,20 +43,38 @@ apt-get install manpages-zh
 
 #Qt
 
+#eclipse
+
+#maven
+apt-get install maven
+
+#protocol-buffer
+apt-get install protobuf-compiler
+
+#Hadoop&Hbase 安装见https://github.com/Will-Grindelwald/Coursework/tree/master/BDMS_2016_spring/hw1/安装与配置, 以下是将hadoop源码转成eclipse项目导入eclipse中
+#apt-get install g++ maven protobuf-compiler=2.5.0 cmake zlib1g-dev findbugs
+#cd hadoop的源码目录
+#cd ./hadoop-maven-plugins
+#mvn install
+#cd ../** 你想构建的项目目录
+#mvn eclipse:eclipse -DskipTests
+
 #jdk  配置：在/etc/profile添加
 #export JAVA_HOME=/usr/local/java/jdk1.8.0_77  *********要改哦
 #export JRE_HOME=${JAVA_HOME}/jre
 #export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 #export PATH=$PATH:${HOME}/bin:${JAVA_HOME}/bin
 
-#Hadoop&Hbase
-
 ##################################
 #小工具
 
-#zsh?
+#zsh
+apt-get install zsh
 
 #配置输入法
+
+echo "开始安装 gksu" #使GUI程序使用sudo
+apt-get install gksu
 
 echo "开始安装 htop"
 apt-get install htop
