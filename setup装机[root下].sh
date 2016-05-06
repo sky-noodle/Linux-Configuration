2f6a7b23fd33168bfca31b08dbdd1ed4dd1ea64d
will <ljclg_1516@foxmail.com>
2016-05-06 11:54:33 +0800

    update

--- setup装机[root下].sh
+++ setup装机[root下].sh
@@ -2,12 +2,12 @@
 
 #终端提示符彩色显示
 echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >> ~/.bashrc
+echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >> /root/.bashrc
 #想改哪个用户的提示符，就放到那个用户的~/.bashrc中，尽量不要动全局变量，如/etc/profile、/etc/bash.bashrc，同时~/.bashrc是最后读取的，不会被overwrite
 #.vimrc同理
 
 #总汇
 #apt-get install build-essential vim-gtk ctags cscope astyle git ssh manpages-zh cmake
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #/etc/init.d/ssh start
 #cd ~; ssh-keygen -t rsa
 #cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys; ssh localhost
@@ -18,7 +18,7 @@ echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >>
 echo "开始安装 Vim & GVim ctags Vundle"
 apt-get install vim-gtk ctags cscope astyle
 #install vimdoc@cn from vimcdoc.sourceforge.net
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "开始安装 git"
 apt-get install git
@@ -27,12 +27,12 @@ apt-get install git
 echo "开始安装 ssh"
 apt-get install ssh    #其实就是openssh(sever&client)
 /etc/init.d/ssh start  #启动ssh
-#cd ~; ssh-keygen -t rsa; cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys; ssh localhost
+cd ~; ssh-keygen -t rsa; cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys; ssh localhost
 
 echo "开始安装 中文man"
 apt-get install manpages-zh
 
-#cmake
+echo "cmake"
 apt-get install cmake
 
 #pycharm
@@ -45,11 +45,11 @@ apt-get install cmake
 
 #eclipse
 
-#maven
-apt-get install maven
+#echo "maven"
+#apt-get install maven
 
-#protocol-buffer
-apt-get install protobuf-compiler
+#echo "protocol-buffer"
+#apt-get install protobuf-compiler
 
 #Hadoop&Hbase 安装见https://github.com/Will-Grindelwald/Coursework/tree/master/BDMS_2016_spring/hw1/安装与配置, 以下是将hadoop源码转成eclipse项目导入eclipse中
 #apt-get install g++ maven protobuf-compiler=2.5.0 cmake zlib1g-dev findbugs
@@ -59,22 +59,22 @@ apt-get install protobuf-compiler
 #cd ../** 你想构建的项目目录
 #mvn eclipse:eclipse -DskipTests
 
-#jdk  配置：在/etc/profile添加
+#jdk  配置：在 /etc/profile 及 .bashrc 添加
 #export JAVA_HOME=/usr/local/java/jdk1.8.0_77  *********要改哦
 #export JRE_HOME=${JAVA_HOME}/jre
 #export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
-#export PATH=$PATH:${HOME}/bin:${JAVA_HOME}/bin
+#export PATH=${PATH}:${HOME}/bin:${JAVA_HOME}/bin
 
 ##################################
 #小工具
 
-#zsh
-apt-get install zsh
+#echo "zsh"
+#apt-get install zsh
 
 #配置输入法
 
-echo "开始安装 gksu" #使GUI程序使用sudo
-apt-get install gksu
+#echo "开始安装 gksu" #使GUI程序使用sudo
+#apt-get install gksu
 
 echo "开始安装 htop"
 apt-get install htop
@@ -82,7 +82,7 @@ apt-get install htop
 echo "开始安装 tree"
 apt-get install tree
 
-#dconf Editor 系统配置编辑器
+#echo "dconf Editor" #系统配置编辑器
 apt-get install dconf-editor
 
 #GUI软件
@@ -93,9 +93,8 @@ apt-get install dconf-editor
 #echo "开始安装 在终端打开"
 #sudo apt-get install nautilus-open-terminal
 
-#点击应用程序 Launcher 图标即可最小化
-#gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+#echo "点击应用程序 Launcher 图标即可最小化"
+gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
 
-#Unity 启动器移动到桌面底部
+#echo "Unity 启动器移动到桌面底部"
 #gsettings set com.canonical.Unity.Launcher launcher-position Bottom
-
