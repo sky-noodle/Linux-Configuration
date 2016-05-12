573eb45fdfd14195c308a0b24a0dc13ecdd5e5d3
will <ljclg_1516@foxmail.com>
2016-05-12 12:23:08 +0800

    update

--- setup装机[root下].sh
+++ setup装机[root下].sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 #终端提示符彩色显示
-echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >> ~/.bashrc
-echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >> /root/.bashrc
-#想改哪个用户的提示符，就放到那个用户的~/.bashrc中，尽量不要动全局变量，如/etc/profile、/etc/bash.bashrc，同时~/.bashrc是最后读取的，不会被overwrite
+sudo echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >> ~/.bashrc
+sudo echo 'export PS1="\[\e[32;1m\][\u@\h:\[\e[34;1m\]\w\[\e[32;1m\]]$>\[\e[0m\]"' >> /root/.bashrc
+#想改哪个用户的提示符, 就放到那个用户的~/.bashrc中, 尽量不要动全局变量, 如/etc/profile、/etc/bash.bashrc, 同时~/.bashrc是最后读取的, 不会被overwrite
 #.vimrc同理
 
 echo "开始安装 build-essential" #1604以上就不需要了
-apt-get install build-essential
+sudo apt-get install build-essential
 
-echo "开始安装 Vim & GVim ctags Vundle"
-apt-get install vim-gtk ctags cscope astyle
+#VSCode                     # 作为主力 GUI 编辑器, 甚至 IDE
+
+echo "开始安装 Vim 套装"    # 作为主力 终端界面的 编辑器, 保证适用简洁高效
+sudo apt-get install vim-gtk ctags cscope astyle
 #install vimdoc@cn from vimcdoc.sourceforge.net
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+wget -q https://raw.githubusercontent.com/Will-Grindelwald/Linux-Configuration/master/Vim/setup && sh -x setup
 
 echo "开始安装 git"
-apt-get install git
+sudo apt-get install git
 #配置ssh keys与gpg keys
 
 echo "开始安装 ssh"
-apt-get install ssh    #其实就是openssh(sever&client)
-/etc/init.d/ssh start  #启动ssh
+sudo apt-get install ssh    #其实就是openssh(sever&client)
+/etc/init.d/ssh start       #启动ssh
 cd ~; ssh-keygen -t rsa; cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys; ssh localhost
 
 echo "开始安装 中文man"
-apt-get install manpages-zh
+sudo apt-get install manpages-zh
 
 echo "cmake"
-apt-get install cmake
-
-#VSCode*************
+sudo apt-get install cmake
 
 #pycharm
 
@@ -40,20 +41,20 @@ apt-get install cmake
 #eclipse
 
 #echo "maven"
-#apt-get install maven
+#sudo apt-get install maven
 
 #echo "protocol-buffer"
-#apt-get install protobuf-compiler
+#sudo apt-get install protobuf-compiler
 
 #Hadoop&Hbase 安装见https://github.com/Will-Grindelwald/Coursework/tree/master/BDMS_2016_spring/hw1/安装与配置, 以下是将hadoop源码转成eclipse项目导入eclipse中
-#apt-get install g++ maven protobuf-compiler=2.5.0 cmake zlib1g-dev findbugs
+#sudo apt-get install g++ maven protobuf-compiler=2.5.0 cmake zlib1g-dev findbugs
 #cd hadoop的源码目录
 #cd ./hadoop-maven-plugins
 #mvn install
 #cd ../** 你想构建的项目目录
 #mvn eclipse:eclipse -DskipTests
 
-#jdk  配置：在 /etc/profile 及 .bashrc 添加
+#jdk  配置: 在 /etc/profile 及 .bashrc 添加
 #export JAVA_HOME=/usr/local/java/jdk1.8.0_77  *********要改哦
 #export JRE_HOME=${JAVA_HOME}/jre
 #export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
@@ -68,21 +69,21 @@ apt-get install cmake
 #小工具
 
 #echo "zsh"
-#apt-get install zsh
+#sudo apt-get install zsh
 
 #配置输入法
 
-#echo "开始安装 gksu" #使GUI程序使用sudo
-#apt-get install gksu
+#echo "开始安装 gksu"       #使GUI程序使用sudo
+#sudo apt-get install gksu
 
 echo "开始安装 htop"
-apt-get install htop
+sudo apt-get install htop
 
 echo "开始安装 tree"
-apt-get install tree
+sudo apt-get install tree
 
-#echo "dconf Editor" #系统配置编辑器
-apt-get install dconf-editor
+#echo "dconf Editor"        #系统配置编辑器
+sudo apt-get install dconf-editor
 
 #GUI软件
 #BleachBit
